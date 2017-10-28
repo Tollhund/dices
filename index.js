668de5cb693d9cdb629f0df36938018adc062f86
Tollhund <kkaleb@gmail.com>
2017-10-28 19:11:24 +0300

    привязка к игроку, проверка количества

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
+// конструктор игрока
 function player(id) {
 	this.ident = "p" + (id + 1);
 	var diceQ = 2;
 	var diceSumm = 0;
     var checkTurn = false;
     var playerDices = [];
-    
+	var pButtonDiv = "";
+	var playerDiv = "";
+    var pDicesDiv = "";
+	
 	this.drawBox = function (plBoxSize) {
 		var container = document.querySelector('#game>#playerBox');
-		var plBox = '<div id="' +
+		playerDiv = document.createElement('div');
+		playerDiv.id = this.ident;
+		playerDiv.className = 'player ' + plBoxSize;
+		container.appendChild(playerDiv);
+		
+		pDicesDiv = document.createElement('div');
+		pDicesDiv.className = "row dices";
+		playerDiv.appendChild(pDicesDiv);
+		
+		drawDices();
+		
+		pButtonDiv = document.createElement('div');
+		pButtonDiv.className = 'button';
+		playerDiv.appendChild(pButtonDiv);
+		
+		// первый вариант кода добавляющий кубики и 								кнопку
+		/*var plBox = '<div id="' + 
 					this.ident +
 					'" class="player' + " " +
 					plBoxSize +
 					'"><div class="row dices">' +
 					drawDices(diceQ) +
 					'</div><div class="button"></div></div>';
-		container.innerHTML += plBox;
+		
+		container.innerHTML += plBox;*/
 	}
-	function drawDices(quantity) {
-		var dicesBox = "";
-		for (var i = 0; i < quantity; i++) {
-			dicesBox += "<div class='dice " +
-				checkSizeDraw(quantity)+ 
-				"'></div>";
+	function drawDices() {
+		for (var i = 0; i < diceQ; i++) {
+			var diceDiv = document.createElement('div');
+			diceDiv.className = "dice " + checkSizeDraw(diceQ);
+			pDicesDiv.appendChild(diceDiv);
 		}
-		return dicesBox;
 	}
 	this.roll = function() {
         if(checkTurn){
@@ -43,8 +62,17 @@ function player(id) {
 	this.getPlayerDices = function(){
 		return playerDices;
 	}
-	this.getDiceQ = function(){
+	this.getDiceQ = function() {
 		return diceQ;
+	}
+	this.getPlayerDiceDiv = function() {
+		return pDicesDiv;
+	}
+	this.getPlayerDiv = function() {
+		return playerDiv;
+	}
+	this.getPlayerButton = function() {
+		return pButtonDiv;
 	}
     this.setDiceQ = function(quantity) {
         diceQ = quantity;
@@ -68,12 +96,14 @@ function player(id) {
             console.log("Бросок игрока " + this.ident + " : " + playerDices[i].value);    
         }
     }
-    this.setSnakeEyes = function () {
-        for(var i = 0; i < playerDices.length; i++){
-            playerDices[i].setValue(playerDices[i].getDiceDiv(), 1);
-        }
-    }
+    //this.setSnakeEyes = function () {
+        //for(var i = 0; i < playerDices.length; i++){
+            //playerDices[i].setValue(playerDices[i].getDiceDiv(), 1);
+        //}
+    //} 
 }
+
+// конструктор кубиков
 function dice() {
 	this.type = "d6";
 	var diceDiv = ""; 
@@ -94,6 +124,7 @@ function dice() {
     }
 }
 
+//создание <div> в зависимости от количества
 function checkSizeDraw(quantity) {
 	switch (quantity) {
 		case 0: 
@@ -140,6 +171,7 @@ function winDisplay(players) {
 	}
 }
 
+//функция кнопки броска кубиков
 function rollEvent(players) {
 	var buttons = document.querySelectorAll('.button');
 	for (var i = 0; i < buttons.length; i++) {
@@ -158,32 +190,38 @@ function rollEvent(players) {
 	}
 }
 
+// функция Drag'n'Drop
 function dragEvent(players) {
-	var playersBoxes = document.querySelectorAll('.player');
-	for (var i = 0; i < playersBoxes.length; i++) {
-		playersBoxes[i].onmousedown = function(e) {
-			var dragBox = document.createElement('div');
-			dragBox.className = "dragBox";
-			
-			document.body.appendChild(dragBox);
-			console.log(dragBox.offsetHeight, dragBox.offsetWidth);
-			
-			dragBox.style.top = e.pageY - dragBox.offsetHeight/2 - 1 + 'px';
-			dragBox.style.left = e.pageX - dragBox.offsetWidth/2 - 1 + 'px';
-			
-			document.onmousemove = function(e) {
+	for (var i = 0; i < players.length; i++) {
+		(function(i){
+			players[i].getPlayerDiv().onmousedown = function(e) {
+			var target = e.target;
+			console.log(players[i].getPlayerButton());
+			if (target != players[i].getPlayerButton()) {
+				var dragBox = document.createElement('div');
+				dragBox.className = "dragBox";
+				dragBox.id = this.id;
+				console.log(dragBox.id);
+				document.body.appendChild(dragBox);
+
 				dragBox.style.top = e.pageY - dragBox.offsetHeight/2 - 1 + 'px';
 				dragBox.style.left = e.pageX - dragBox.offsetWidth/2 - 1 + 'px';
-			}
-			
-			dragBox.onmouseup = function(e) {
-				if(e.pageX > compareWin.rect.left && e.pageX < compareWin.rect.right && e.pageY > compareWin.rect.top && e.pageY < compareWin.rect.bottom) {	
-				} else {
+
+				document.onmousemove = function(e) {
+					dragBox.style.top = e.pageY - dragBox.offsetHeight/2 - 1 + 'px';
+					dragBox.style.left = e.pageX - dragBox.offsetWidth/2 - 1 + 'px';
+				}
+
+				dragBox.onmouseup = function(e) {
+					if(e.pageX > compareWin.rect.left && e.pageX < compareWin.rect.right && e.pageY > compareWin.rect.top && e.pageY < compareWin.rect.bottom) {
+						compareWin.drawPlayer(dragBox.id);
+					}
+
 					var start = Date.now();
 					var opac = 1;
 					var timer = setInterval(function(){
 						var timedPassed = Date.now() - start;
-						
+
 						if(timedPassed >= 400) {
 							clearInterval(timer);
 							document.body.removeChild(dragBox);
@@ -191,16 +229,18 @@ function dragEvent(players) {
 						opac -= 0.1;
 						dragBox.style.opacity = opac;
 					},40);
+
+					document.onmousemove = null;
+					dragBox.onmouseup = null;
+
 				}
-
-				document.onmousemove = null;
-				dragBox.onmouseup = null;
-
 			}
 		}
+		})(i);
 	}
 }
 
+//findParentBySelector function
 function collectionHas(a, b) { //helper function (see below)
     for(var i = 0, len = a.length; i < len; i ++) {
         if(a[i] == b) return true;
@@ -216,24 +256,58 @@ function findParentBySelector(elm, selector) {
     return cur; //will return null if not found
 }
 
+// выяснение победителя
 var compareWin = {
-        div: document.getElementById('compareTwo'),
-		
-        rect: "", // объект с координатами поля
-		
-		box: "",
-		
-		colBox: function() {
-			var box = '<div class="compBox col-6"></div>';
-            compareWin.div.innerHTML += box;
-			compareWin.box = document.querySelector(".compBox");
-			compareWin.setRect();
-        },
-		setRect: function() {
-			compareWin.rect = compareWin.box.getBoundingClientRect();
-		}
-    }
+	div: document.getElementById('compareTwo'),
 
+	rect: {}, // объект с координатами поля
+
+	box: "",
+	
+	compPlayersArr: [],
+
+	colBox: function() {
+		var box = '<div class="compBox"></div>';
+		compareWin.div.innerHTML += box;
+		compareWin.box = document.querySelector(".compBox");
+		compareWin.setRect();
+	},
+	setRect: function() {
+		compareWin.rect = compareWin.box.getBoundingClientRect();
+	},
+	
+	checkUnic: function(id) {
+		for (var i = 0; i < compareWin.compPlayersArr.length; i++) {
+			if (compareWin.compPlayersArr[i].id == id) {
+				return false;
+			}
+		}
+		return true;
+	},
+	
+	checkSize: function() {
+		var arrL = compareWin.compPlayersArr;
+		var divSplit;
+		for (var i = 0; i < arrL.length; i++) {
+			divSplit = arrL[i].className.split(" ");
+			divSplit[0] = checkSizeDraw(arrL.length);
+			arrL[i].className = divSplit.join(" ");
+		}
+	},
+	
+	drawPlayer: function(dragPlayerId) {
+		if (compareWin.checkUnic(dragPlayerId)) {
+			var playerDiv = document.createElement('div');
+			playerDiv.className = 'col-12 comparePlayer';
+			playerDiv.id = dragPlayerId;
+			compareWin.box.appendChild(playerDiv);
+			compareWin.compPlayersArr.push(playerDiv);
+			compareWin.checkSize();
+		}
+	},
+}
+
+// main
 function game(){
 	var players = [];
 	var plBoxSize = "";
